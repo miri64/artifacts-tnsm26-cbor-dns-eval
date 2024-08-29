d76befedba63f51585b5f44530feb115132e8eac
Martine S. Lenders <martine-lenders@tu-dresden.de>
2024-08-29 15:39:18 +0000

    ubuntu-setup.sh: remove obsoleted python dependency

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -12,7 +12,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade curl git parallel pi
     npm python3-pip python3-virtualenv python3-dev \
     libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
     libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
-    lzma lzma-dev tk-dev uuid-dev zlib1g-dev libmpdec-dev poppler-utils \
+    lzma lzma-dev tk-dev uuid-dev zlib1g-dev poppler-utils \
     cm-super dvipng texlive-fonts-extra texlive-latex-extra texlive-pictures texlive-xetex
 yes | sudo DEBIAN_FRONTEND=teletype dpkg-reconfigure wireshark-common
 sudo usermod -a -G wireshark ${USER}
